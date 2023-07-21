834dc79e0f43db5e65ed9bf937f022845e072c2d
AdautoMarques <adautomarques937@gmail.com>
2023-07-20 22:53:59 -0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,33 @@
+
+
+const key = "ca2569c639df9e1a849f5873b617129a";
+
+
+function listarDados(dados) {
+    console.log(dados);
+
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp)+"º C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML ="Umidade : " + dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+
+}
+
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`)
+    .then(resposta => resposta.json())
+
+    listarDados(dados) 
+   
+}
+
+
+function clickBotao() {
+
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
